3102a6ed551d9fcbab65c33da8830b204b5178aa
Wikten Alves <wiktenalves@gmail.com>
2024-02-12 00:29:29 -0300

    exercícios 3 e 4 feitos

--- ex002.py
+++ ex002.py
@@ -1,2 +1,10 @@
+'''
 nome = input("Digite seu nome: ")
 print("É um prazer te conhecer,", nome+'!')
+'''
+#Desafio extra
+
+n1 = int(input('Digite o primeiro número: '))
+n2 = int(input('Digite o segundo número: '))
+
+print(f'A soma entre {n1} e {n2} é {n1+n2}')
---
+++ ex003.py
@@ -0,0 +1,5 @@
+n1 = int(input('Digite o primeiro número: '))
+n2 = int(input('Digite o segundo número: '))
+
+print(f'O valor de {n1} mais {n2} é {n1+n2}')
+
