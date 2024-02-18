dbdabe0b3fe88617b55bbf4b89c595974bb7800f
Wikten Alves <wiktenalves@gmail.com>
2024-02-18 12:13:52 -0300

    Resumo aula 09 feito

---
+++ conteudoAulas/aula09.py
@@ -0,0 +1,44 @@
+frase = 'Curso em Vídeo Python'
+#print(frase[3]) Mostra a letra
+
+#print(frase[3:15]) Mostra apartir do primeiro índice apte o sgundo índice
+
+#print(frase[3:15:2]) pulando de dois em dois
+
+#print(frase[::2]) Mostra a string toda pulando de 2 em 2
+
+#print(frase.count('o')) Conta as letras o da string
+
+#print(frase.upper().count('o')) Conta as letras O da string com todas as letras maiúsculas.
+
+#print(len(frase)) conta a quantidade de catacteres de uma string
+
+#print(len(frase.strip())) conta a quantidade de catacteres de uma string removdendo espaços extras no fim e on início
+
+#print(frase.replace('Python', 'Android')) Substitui a palavra Python por Android.
+
+#frase = frase.replace('Python', 'Android') Salva a frase modificada com replace dentro da variavel
+
+#print('curso' in frase) Verifica se a palavra curso está contida na string frase e retorna true ou false
+
+#print(frase.find('Curso')) Mostra  o índice de inicio da palavra na string, e se n houver retorna -1
+
+#print(frase.split()) Divide as palavras da string e as joga dentro de uma lista.
+
+#dividido = frase.split()
+#print(dividido[2]) Exibe a palavra de índice 2 da lista criada com split
+
+#print(dividido[2][3]) Exibe a palavra de índice 2 da lista e índice 3 da palavra escolhida.
+
+
+
+
+
+
+
+
+
+
+
+#print("""Serve para escrever um texto inteiro quando o mesmo é muito grande e fica em uma maneira prática.""")
+
