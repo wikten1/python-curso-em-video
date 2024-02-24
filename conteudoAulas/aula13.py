16ac330cd7aa87ef9109ef441dd668cb184077dd
Wikten Alves <wiktenalves@gmail.com>
2024-02-24 17:57:13 -0300

    Resumo aula 13 completo

--- conteudoAulas/aula13.py
+++ conteudoAulas/aula13.py
@@ -24,3 +24,31 @@ for c in range(0, 7, 2):
     print(c)
 print('fim')
 '''
+
+#Determinando o número final
+'''
+n = int(input('Digite um número inteiro: '))
+
+for c in range(0, n):
+    print(c+1)
+print('Fim!')
+'''
+
+#Determinando tudo
+'''
+inicio = int(input('Digite o número de início: '))
+fim = int(input('Digite o número de fim: '))
+passo = int(input('Digite o passo: '))
+
+for c in range(inicio, fim, passo):
+    print(c)
+print('Fim!')
+'''
+
+#Repetição de comandos com números determinados
+s = 0
+for c in range(0,4):
+    n = int(input('Digite um número: '))
+    s += n
+print(f'O somatório dos números é: {s}')
+print('FIM!')
