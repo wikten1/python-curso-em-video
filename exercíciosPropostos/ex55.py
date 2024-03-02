cc8593602382868a3b6693bfcfe301ffd9aa5b9d
Wikten Alves <wiktenalves@gmail.com>
2024-03-02 01:00:00 -0300

    Modificação exercício 55

--- exercíciosPropostos/ex55.py
+++ exercíciosPropostos/ex55.py
@@ -1,14 +1,18 @@
 maiorPeso = 0
-menorPeso = 1000
+menorPeso = 0
 
 for i in range(1,6):
     peso = float(input(f'Digite o peso da {i}° pessoa: '))
     
-    if peso > maiorPeso:
+    if i == 1:
         maiorPeso = peso
-        
-    if peso < menorPeso:
         menorPeso = peso
+    else:
+        if peso > maiorPeso:
+            maiorPeso = peso
+            
+        if peso < menorPeso:
+            menorPeso = peso
 
 print(f'O maior peso registrado foi: {maiorPeso:.2f}')
 print(f'O menor peso registrado foi: {menorPeso:.2f}')
