cd64cb5f2b05b6780b24ac8c4749ef320809d995
Wikten Alves <wiktenalves@gmail.com>
2024-03-13 11:20:01 -0300

    ex 63 consertado

--- exercíciosPropostos/ex65.py
+++ exercíciosPropostos/ex65.py
@@ -1,9 +1,22 @@
 soma = 0
 resp = 'S'
+maior = 0
+menor = 99999999
 cont = 0
 while resp != 'N':
     num = int(input('Digite um número: '))
     soma += num
+    
+    if num > maior:
+        maior = num
+    
+    if menor > num:
+        menor = num
+        
     cont += 1
     resp = input('Deseja continuar [S/N]: ').upper()[0]
+    
 print(f'A soma dos {cont} números é: {soma}')
+print(f'O maior número digitado é: {maior}')
+print(f'O menor número digitado é: {menor}')
+print(f'A média dos números digitados é: {soma/cont:.2f}')
