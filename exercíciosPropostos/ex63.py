da2690faf4c4711c07ab48dd4f01ef3b5d94d124
Wikten Alves <wiktenalves@gmail.com>
2024-03-13 00:58:49 -0300

    Exercício 63 feito

--- exercíciosPropostos/ex63.py
+++ exercíciosPropostos/ex63.py
@@ -1,1 +1,14 @@
 num = int(input('Digite um número inteiro: '))
+
+t1 = 0
+t2 = 1
+cont = 3
+print(f'{t1}, {t2}', end='')
+cont = 3
+while cont <= num:
+    t3 = t1 + t2
+    print(f', {t3}', end='')
+    t1 = t2
+    t2 = t3
+    cont += 1
+print("\nFim!")
