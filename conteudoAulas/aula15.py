7cee04579b57f4b83ed8d80e23d6d11d345fa770
Wikten Alves <wiktenalves@gmail.com>
2024-03-16 23:34:45 -0300

    aula 15 fazendo

---
+++ conteudoAulas/aula15.py
@@ -0,0 +1,12 @@
+'''
+cont = 1
+while cont <= 10:
+    print(cont, '->', end='')
+    cont += 1
+print('Acabou')
+'''
+
+n = cont = 0
+while cont < 3: 
+    n = int(input('Digite um número: '))
+    cont += 1
