f89b01aa95f66501ecaa954e906b7f8f7ac11dc4
Wikten Alves <wiktenalves@gmail.com>
2024-03-17 23:19:32 -0300

    Resumo aula 15 feito

--- conteudoAulas/aula15.py
+++ conteudoAulas/aula15.py
@@ -5,8 +5,17 @@ while cont <= 10:
     cont += 1
 print('Acabou')
 '''
-
+'''
 n = cont = 0
 while cont < 3: 
     n = int(input('Digite um número: '))
     cont += 1
+    '''
+n = s = 0
+while True:
+    n = int(input('Digite um número: '))
+    if n == 999:
+        break
+    s += n
+print(f'A soma vale:3 {s}')
+        
