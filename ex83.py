e9a9dc80d5f26651131a55fb978ebe4cce668985
Wikten Alves <wiktenalves@gmail.com>
2024-04-22 15:48:08 -0300

    Desafios feitos
    Desafios do 80 ao 82 feitos

---
+++ ex83.py
@@ -0,0 +1,15 @@
+expressao = input("Digite a sua expressão: ")
+pilha = []
+for simb in expressao:
+    if simb == '(':
+        pilha.append('(')
+    elif simb == ')':
+        if len(pilha) > 0:
+            pilha.pop()
+        else:
+            pilha.append(')')
+            break
+if len(pilha) == 0:
+    print('Sua expressão está válida!')
+else:
+    print('Sua expressão está errada!')
